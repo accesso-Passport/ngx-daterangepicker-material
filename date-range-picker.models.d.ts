c755b8ed8d005a40eb7a45a9436b707c66e6a642
David Smith <david.smith@accesso.com>
2021-08-13 17:11:45 -0400

    update to push to te2 artifactory npm registry

--- date-range-picker.models.d.ts
+++
@@ -1,9 +0,0 @@
-import * as moment from 'moment';
-export interface DateRangePreset {
-    key: string;
-    label: string;
-    range: {
-        start: moment.Moment;
-        end: moment.Moment;
-    };
-}
