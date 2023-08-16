f1913766f07773433953719007f9845a8a3a9a94
Corey Sherman <corey.sherman@accesso.com>
2023-08-16 10:18:38 -0400

    PSP-4089 - switch from moment to dayjs

--- src/daterangepicker/directives/date-range-picker.directive.ts
+++ src/daterangepicker/directives/date-range-picker.directive.ts
@@ -20,7 +20,7 @@ import {
 } from '@angular/core';
 import { DateRangePickerComponent } from '../components/date-range-picker.component';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
-import dayjs from 'dayjs';
+import * as dayjs from 'dayjs';
 import { LocaleConfig } from '../date-range-picker.config';
 import { LocaleService } from '../services/locale.service';
 
