19027ea7736d47e24d5acef737124f3a59c5489a
Corey Sherman <corey.sherman@accesso.com>
2025-02-04 12:11:45 -0500

    PSP-5248 - upgrade angular

---
+++ src/daterangepicker/dynamic-date.pipe.ts
@@ -0,0 +1,18 @@
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'date'
+})
+export class DynamicDatePipe implements PipeTransform {
+	constructor(private datePipe: DatePipe) {}
+
+	transform(
+		value: any,
+		format?: string,
+		timezone?: string,
+		locale?: string
+	): string | null {
+		return this.datePipe.transform(value, format, timezone, locale);
+	}
+}
